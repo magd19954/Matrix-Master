b36fcae2dd9afd6af2f4e0abd1fae418052e20fc
magd19954 <majd.albatran@gmail.com>
2025-03-27 23:15:19 +0100

    helooooooool

--- nodejs/express/config/mongoose.js
+++ nodejs/express/config/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbURI = 'mongodb+srv://majdalbatran:<db_password>@cluster0.p1q97gn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
+const dbURI = 'mongodb+srv://majdalbatran:11510289@cluster0.rvkbypb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 // Replace <db_password> with your actual MongoDB password
 mongoose.connect(dbURI)
     .then(() => {
